a6692546bd1efd350fdd882395dcce90a98dc83a
ryanbirdi97 <ryanbirdi97@icloud.com>
2023-01-06 16:11:59 +0000

    3) Added search function

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,39 @@ import './App.css'
 function App() {
 
   const [areas, setAreas] = useState([]);
+  const [outcode, setOutcode] = useState("");
+  const [subitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
 
-  const load = async () => {
-    try {
-      const areaData = await getAreaData()
-  
-      setAreas(areaData);
-    } catch (error) {
-      window.alert("todo: fix app")
-    }
-  }
 
-  useEffect(() => {
-    load();
-  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget)
+    setSubmitted(true)
+    getAreaData(outcode).then((res) => {
+      setAreas(res);
+      setError(false)
+    }).catch((err)=>{
+      if(err) setError(true)
+    })
+  };
 
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={(e) => {
+              setSubmitted(false)
+              setAreas([])
+              setOutcode(e.target.value);
+            }}
+          ></input>
+          <button type="submit">Search Postcode</button>
+        </form>
+        {subitted && areas.length > 0 ? <h2>{`Areas for ${outcode.toUpperCase()}: ${areas.length}`}</h2> : <></>}
+        {error ? <p>Invalid postcode</p> : <></>}
     </div>
   )
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const getAreaData = async () => {
-  const { data } = await axios.get("https://api.zippopotam.us/GB/bb10");
+export const getAreaData = async (outcode) => {
+  const { data } = await axios.get(`https://api.zippopotam.us/GB/${outcode}`);
   return data.places;
 };
